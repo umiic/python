a0eb6f6ac6d6f962262811d5859f31564b8f60d4
umiic <18ucheema@mtsn.org.uk>
2022-07-04 21:47:43 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,5 +3,15 @@ if(za_waruldo):
     print("Explosion!")
 else:
     print("Subarashi...")
-for i in range(1, 11, 1):
+for i in range(1, 131, 2):
     print(i)
+    
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+print(areas[2])
+# Print out last element from areas
+print(areas[9])
+
+# Print out the area of the living room
+print(areas[5])    
